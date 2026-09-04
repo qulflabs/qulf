87a050da41d4ef7682fc53184cbc5f373a221cdd
peter <peterkinfe548@gmail.com>
2026-09-04 17:37:29 +0300

    feat(providers): exported discord provider in public API

--- src/qulf/providers/__init__.py
+++ src/qulf/providers/__init__.py
@@ -1,9 +1,11 @@
 from qulf.providers.base import BaseOAuthProvider, OAuthTokenResponse, OAuthUserProfile
+from qulf.providers.discord import DiscordProvider
 from qulf.providers.github import GitHubProvider
 from qulf.providers.google import GoogleProvider
 
 __all__ = [
     "BaseOAuthProvider",
+    "DiscordProvider",
     "GitHubProvider",
     "GoogleProvider",
     "OAuthTokenResponse",

e600f98a506f41868455dd05a5946f77b3640fb6
peter <peterkinfe548@gmail.com>
2026-09-04 18:15:18 +0300

    feat(providers): exported AppleProvider in providers module

--- src/qulf/providers/__init__.py
+++ src/qulf/providers/__init__.py
@@ -1,9 +1,11 @@
+from qulf.providers.apple import AppleProvider
 from qulf.providers.base import BaseOAuthProvider, OAuthTokenResponse, OAuthUserProfile
 from qulf.providers.discord import DiscordProvider
 from qulf.providers.github import GitHubProvider
 from qulf.providers.google import GoogleProvider
 
 __all__ = [
+    "AppleProvider",
     "BaseOAuthProvider",
     "DiscordProvider",
     "GitHubProvider",

b0e6b9fe8335cded6f66af22513e744e503578f5
k4l3b4 <kaleba647@gmail.com>
2026-09-21 23:35:50 +0300

    refactor(cli): reorganize template files into separate modules

---
+++ src/qulf/cli/templates/django.py
@@ -0,0 +1,7 @@
+"""
+Qulf Database Models (Django ORM)
+Generated on: {timestamp}
+"""
+
+# dummy file to silence errors for now, 
+# will be replaced with actual scaffolding in the next pull request
